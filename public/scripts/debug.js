74f6854207fa3e08dd33b8fdee57a8e8cb301fa2
frissdiegurke <frissdiegurke@hotmail.com>
2015-11-16 10:16:34 +0100

    Fix wrong settings construction

--- public/scripts/debug.js
+++ public/scripts/debug.js
@@ -2,17 +2,17 @@
 
 define("@{type.name}/@{id}/debug", function () {
   var __slice = Array.prototype.slice;
-  var env = "@{env}", dbg = (env === "development");
+  var env = "@{env}", dev = (env === "development");
 
   var exports = {
-    enabled: dbg,
+    enabled: dev,
     _log: function () {
       var args = 1 <= arguments.length ? __slice.call(arguments, 0) : [];
       console.log.apply(console, ["@{name} DEBUG -"].concat(__slice.call(args)));
     },
     log: function () {
       var args = 1 <= arguments.length ? __slice.call(arguments, 0) : [];
-      if (dbg) { exports._log.apply(null, args); }
+      if (dev) { exports._log.apply(null, args); }
     },
     error: function () {
       console.error.apply(console, ["@{name} ERROR -"].concat(__slice.call(arguments)));
