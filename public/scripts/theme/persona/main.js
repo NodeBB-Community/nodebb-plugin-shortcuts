9cf3c96ffdaebd90e9741a1d759e9cec90b2fd1d
frissdiegurke <frissdiegurke@hotmail.com>
2015-11-16 19:22:43 +0100

    Add persona selection definitions

--- public/scripts/theme/persona/main.js
+++ public/scripts/theme/persona/main.js
@@ -2,7 +2,13 @@
 
 /*
  * Latest functionality approval:
- * TODO add theme support
+ *   2015/11/16
+ *   NodeBB 0.9.0
+ *   nodebb-theme-lavender 4.0.36
  */
 
-define("@{type.name}/@{id}/themes/persona/main", function () { return $.noop; });
+define("@{type.name}/@{id}/themes/persona/main", [
+  "@{type.name}/@{id}/themes/persona/selection"
+], function (selection) {
+  return function (shortcuts, theme) { selection(shortcuts, theme); };
+});
---
+++ public/scripts/theme/persona/selection.js
@@ -0,0 +1,112 @@
+"use strict";
+
+define("@{type.name}/@{id}/themes/persona/selection", ["@{type.name}/@{id}/selection/Area"], function (Area) {
+  return function (shortcuts, theme) {
+    theme.selection = {
+      posts: {
+        selector: "[component=\"post\"]",
+        follow: [
+          function () { ajaxify.go("topic/" + ajaxify.data.slug + "/" + (1 + this.data("index"))); }
+        ]
+      },
+      topics: {
+        selector: "[component=\"category/topic\"]",
+        follow: [
+          function () {
+            var $link = this.find(".teaser a[href*=\"/topic/\"]");
+            if (!$link.length) { $link = $("[component=\"topic/header\"]>a", this); }
+            if ($link.length) { $link[0].click(); }
+          },
+          function () {
+            var $link = this.find("[component=\"topic/header\"]>a");
+            if ($link.length) { $link[0].click(); }
+          }
+        ]
+      },
+      categories: {
+        selector: "[component=\"categories/category\"], .subcategories>[data-cid]",
+        follow: [
+          function () { ajaxify.go("category/" + this.data("cid")); }
+        ]
+      },
+      notifications: {
+        selector: ".notifications-list>[component=\"notifications/item\"]",
+        follow: [
+          function () {
+            var $link = this.find("[component=\"notifications/item/link\"]");
+            if ($link.length) { $link[0].click(); }
+          }
+        ]
+      },
+      users: {
+        selector: ".users-container>li",
+        follow: [
+          function () {
+            var $link = this.find("a[href*=\"/user/\"]");
+            if ($link.length) { $link[0].click(); }
+          }
+        ]
+      },
+      groups: {
+        selector: "[component=\"groups/summary\"]",
+        getClassElement: function () { return this.children().eq(0); },
+        follow: [
+          function () {
+            var $link = this.find(".panel-heading");
+            if ($link.length) { $link[0].click(); }
+          }
+        ]
+      },
+      tags: {
+        selector: ".tag-list>.tag-container",
+        follow: [
+          function () {
+            var $link = this.find(">a");
+            if ($link.length) { $link[0].click(); }
+          }
+        ],
+        getClassElement: function () { return this.children().eq(0); }
+      },
+      chats_recent: {
+        selector: "[component=\"chat/recent\"]>li",
+        getClassElement: function () { return this.find(".user-icon,.username"); },
+        follow: [
+          function () { this[0].click(); }
+        ]
+      },
+      dropDowns: {
+        selector: "[data-toggle=\"dropdown\"]:not([disabled])",
+        getArea: function () {
+          if (this.parent().is("#header-menu *")) { return false; }
+          var area = new Area(this.parent());
+          area.setHooks({
+            selector: ">ul>li:not(.divider,.dropdown-header)",
+            focus: theme.selection.dropDowns.focus,
+            blur: theme.selection.dropDowns.blur,
+            follow: theme.selection.dropDowns.follow
+          });
+          return area;
+        },
+        focus: {
+          area: function () {
+            if (!this.parent.hasClass("open")) { $("[data-toggle=\"dropdown\"]:not([disabled])", this.parent).click(); }
+            setTimeout(theme.utils.blurFocus, 50);
+          },
+          item: function () {
+            var grandpa = this.parent().parent();
+            if (!grandpa.hasClass("open")) { $("[data-toggle=\"dropdown\"]:not([disabled])", grandpa).click(); }
+            setTimeout(theme.utils.blurFocus, 50);
+          }
+        },
+        blur: {
+          area: function () {
+            if (this.parent.hasClass("open")) { $("[data-toggle=\"dropdown\"]:not([disabled])", this.parent).click(); }
+          }
+        },
+        follow: [
+          function () { $(">*", this).focus()[0].click(); }
+        ]
+      }
+    };
+  };
+});
